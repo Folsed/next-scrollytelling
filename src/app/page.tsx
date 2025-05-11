217b99fd36441786023bd081a8f2bbddb5e5ec14
Kyrylo <dexter37814@gmail.com>
2025-05-11 20:47:36 +0300

    feat: add smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-    return <div className=''></div>
+'use client'
+import { useEffect } from 'react'
+
+const HomePage = () => {
+    useEffect(() => {
+        ;(async () => {
+            const LocomotiveScroll = (await import('locomotive-scroll')).default
+            const locomotiveScroll = new LocomotiveScroll()
+        })()
+    }, [])
+    return (
+        <div className='flex flex-col gap-6'>
+            <div className='h-screen w-full bg-amber-400'></div>
+            <div className='h-screen w-full bg-amber-400'></div>
+            <div className='h-screen w-full bg-amber-400'></div>
+            <div className='h-screen w-full bg-amber-400'></div>
+        </div>
+    )
 }
+export default HomePage
